d6aea7a1f30b2b7629a897a9764439650cde451a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2025-01-18 19:48:06 -0600

    working on index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,15 @@ const dpr = window.devicePixelRatio || 1;
 
 canvas.width = 1024 * dpr;
 canvas.height = 576 * dpr;
+
+const layersData = {
+  l_New_Layer_1: l_New_Layer_1,
+  l_New_Layer_2: l_New_Layer_2,
+  l_New_Layer_8: l_New_Layer_8,
+  l_New_Layer_3: l_New_Layer_3,
+  l_Decorations: l_Decorations,
+  l_New_Layer_5: l_New_Layer_5,
+  l_New_Layer_6: l_New_Layer_6,
+  l_New_Layer_9: l_New_Layer_9,
+  l_New_Layer_9_1: l_New_Layer_9_1,
+};
