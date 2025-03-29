cea05b1a2509a1294e0c65d41f20efb11eb18807
DDeShon <36036284+DDeShon@users.noreply.github.com>
2025-03-29 03:18:43 -0500

    working on index.js

--- js/index.js
+++ js/index.js
@@ -143,6 +143,7 @@ function animate(backgroundCanvas) {
   // Render scene
   c.save();
   c.scale(dpr, dpr);
+  c.translate();
   c.clearRect(0, 0, canvas.width, canvas.height);
   c.drawImage(backgroundCanvas, 0, 0);
   player.draw(c);
