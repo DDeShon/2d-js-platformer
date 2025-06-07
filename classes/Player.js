4a21e6e22f309429619b98be44e074009d3da1ad
DDeShon <36036284+DDeShon@users.noreply.github.com>
2025-06-07 00:37:32 -0500

    working on player.js

--- classes/Player.js
+++ classes/Player.js
@@ -51,6 +51,7 @@ class Player {
 
     // Update animation frames
     this.elapsedTime += deltaTime;
+    const secondsInterval = 0.1;
     if (this.elapsedTime > 0.1) {
       this.currentFrame = (this.currentFrame + 1) % 4;
     }
