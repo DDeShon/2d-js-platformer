e36094b17c490890dd18633abafa68ea4cf94a48
DDeShon <36036284+DDeShon@users.noreply.github.com>
2025-08-11 06:32:58 -0500

    working on index.js

--- js/index.js
+++ js/index.js
@@ -125,6 +125,13 @@ const player = new Player({
   velocity: { x: 0, y: 0 },
 });
 
+const Opossum = new Opossum({
+  x: 100,
+  y: 100,
+  size: 16,
+  velocity: { x: 0, y: 0 },
+});
+
 const keys = {
   w: {
     pressed: false,
