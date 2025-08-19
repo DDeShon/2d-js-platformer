ec4eeec0399e185320c702daf1eb97191ea74df2
DDeShon <36036284+DDeShon@users.noreply.github.com>
2025-08-19 06:16:41 -0500

    working on utils.js

--- js/utils.js
+++ js/utils.js
@@ -7,3 +7,9 @@ const loadImage = (src) => {
     })
 }
 
+function checkCollisions(object1, object2) {
+    return (
+        object1.x + object1.width >= object2.x && 
+        object1.x <= object2.x + object2.width
+    )
+}
