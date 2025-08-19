d380cdb91de852bb92e37833fc177e7aa07d3126
DDeShon <36036284+DDeShon@users.noreply.github.com>
2025-08-19 06:14:03 -0500

    working on index.js

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+const loadImage = (src) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = reject;
+        img.src = src;
+    })
+}
+
