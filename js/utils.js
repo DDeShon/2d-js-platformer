162f729535e6cc1a62efeb065285a50d1ca24b7f
DDeShon <36036284+DDeShon@users.noreply.github.com>
2025-08-20 06:34:43 -0500

    working on utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,16 @@
 const loadImage = (src) => {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = reject;
-        img.src = src;
-    })
-}
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+  });
+};
 
 function checkCollisions(object1, object2) {
-    return (
-        object1.x + object1.width >= object2.x && 
-        object1.x <= object2.x + object2.width
-    )
+  return (
+    object1.x + object1.width >= object2.x &&
+    object1.x <= object2.x + object2.width &&
+    object1.y <= object2.y + object2.height
+  );
 }
