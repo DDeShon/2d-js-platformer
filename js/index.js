32578ada5fdef84fd1d5685f484fed8d19b54c4a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2026-01-09 11:32:47 -0600

    working on index.js

--- js/index.js
+++ js/index.js
@@ -298,12 +298,6 @@ function init() {
       width: 36,
       height: 28,
     }),
-    new Oposum({
-      x: 1150,
-      y: 515,
-      width: 36,
-      height: 28,
-    }),
   ];
 
   sprites = [];
